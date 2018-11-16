b16a23fe46efb75fded6f4057065621d7d3db6e8
zack <zbhne@icloud.com>
2018-11-17 07:55:46 +0800

    Initial Commit

---
+++ ShadingDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShadingDemo
+//
+//  Created by lowe on 2018/11/17.
+//  Copyright © 2018 lowe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
